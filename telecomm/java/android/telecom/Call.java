CyanogenMod/android_frameworks_base

IMS: Conference URI support.
Add Telephony extras EXTRAS_IS_CONFERENCE_URI,
EXTRA_DIAL_CONFERENCE_URI which will require by Telecomm,
TeleService, Telephony, Framework, IMS framework etc.

IMS: Allow placeCall with complete URI

Add extra "org.codeaurora.extra.SKIP_SCHEMA_PARSING".
Application need to set the intent extra to dial with
complete uri.

IMS: Add participant support.

Add Phone Capabilities ADD_PARTICIPANT and
Telephony Property ADD_PARTICIPANT_KEY.

IMS: Allow add participant with normal IMS call

We should allow add participant with normal IMS
call to make it conference.
Send add participant through existing connection
of normal IMS call.

IMS: Add Participant support.

Do not create new connection while adding participant
with existing call. Rather send add participant request
through IMSConference.

CRs-Fixed: 1023216
Change-Id: I5052710a2d11a57331bdfbe64247e6a39bf9147a

telecomm/java/android/telecom/Call.java
@@ -262,8 +262,14 @@ public final class Call {
          */
         public static final int CAPABILITY_VOICE_PRIVACY = 0x01000000;
 
+        /**
+         * Add participant in an active or conference call option
+         * @hide
+         */
+        public static final int CAPABILITY_ADD_PARTICIPANT = 0x02000000;
+
         //******************************************************************************************
-        // Next CAPABILITY value: 0x02000000
+        // Next CAPABILITY value: 0x04000000
         //******************************************************************************************
 
         /**
@@ -421,6 +427,9 @@ public final class Call {
             }
             if (can(capabilities, CAPABILITY_VOICE_PRIVACY)) {
                 builder.append(" CAPABILITY_VOICE_PRIVACY");
+            }
+            if (can(capabilities, CAPABILITY_ADD_PARTICIPANT)) {
+                builder.append(" CAPABILITY_ADD_PARTICIPANT");
             }
             builder.append("]");
             return builder.toString();

GlazeOS/frameworks_base

 Conflicts:
	telecomm/java/android/telecom/Call.java
	telecomm/java/android/telecom/Conference.java
Signed-off-by: NoelMacwan <yulespencer@gmail.com>

telecomm/java/android/telecom/Call.java
@@ -255,8 +255,14 @@ public final class Call {
          */
         public static final int CAPABILITY_CAN_PULL_CALL = 0x00800000;
 
+        /**
+         * Add participant in an active or conference call option
+         * @hide
+         */
+        public static final int CAPABILITY_ADD_PARTICIPANT = 0x01000000;
+
         //******************************************************************************************
-        // Next CAPABILITY value: 0x01000000
+        // Next CAPABILITY value: 0x02000000
         //******************************************************************************************
 
         /**
@@ -415,6 +421,9 @@ public final class Call {
             }
             if (can(capabilities, CAPABILITY_CAN_PULL_CALL)) {
                 builder.append(" CAPABILITY_CAN_PULL_CALL");
+            }
+            if (can(capabilities, CAPABILITY_ADD_PARTICIPANT)) {
+                builder.append(" CAPABILITY_ADD_PARTICIPANT");
             }
             builder.append("]");
             return builder.toString();